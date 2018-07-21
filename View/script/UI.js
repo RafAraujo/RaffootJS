87a6f78fe7fd469eb5d8b1563cb9c9a1e7fc18ef
Rafael Araujo <raff-araujo@hotmail.com>
2018-07-20 21:08:11 -0300

    Interface init

---
+++ View/script/UI.js
@@ -0,0 +1,13 @@
+function fillSelect(id, array) {
+    let select = document.getElementById(id);
+    clearSelect(select);
+    select.appendChild(new Option());
+    for (let i = 0; i < array.length; i++) {
+        let object = array[i];
+        select.appendChild(new Option(object.name, object.name));
+    }
+}
+
+function clearSelect(select) {
+    select.options.length = 0;
+}
