2bf73f2761b4cebde4cffc76f48c207561775812
Rafael Araujo <raff-araujo@hotmail.com>
2018-07-29 15:03:16 -0300

    Complementing NewGameController

--- js/app/controllers/NewGameController.js
+++ js/app/controllers/NewGameController.js
@@ -1,18 +1,29 @@
 class NewGameController {
     constructor() {
         this._selectCountries = $('#countries');
+        this._selectClubs = $('#clubs');
+        this._inputName = $('#name');
 
         this._game = new Bind(new Game(), new NewGameView(), 'seed', 'country');
         this._game.seed();
 
-        this._selectCountries.addEventListener('change', this.setCountry.bind(this), {passive: true} );
+        this._selectCountries.addEventListener('change', this._setCountry.bind(this), {passive: true} );
+        this._selectClubs.addEventListener('change', this._setClub.bind(this), {passive: true} );
     }
 
-    setCountry() {
-        this._game.country = this._game.countries.find(c => c.name == this._selectCountries.value);
+    _setCountry() {
+        this._game.country = this._game.countries.find(c => c.name === this._selectCountries.value);
     }
 
-    setClub() {
+    _setClub() {
+        this._game.club = this._game.country.clubs.find(c => c.name === this._selectClubs.value && c.country === this._game.country);
+    }
 
+    _setName() {
+        this._game.name = this._inputName.value;
+    }
+
+    save() {
+        this._setName();
     }
 }
