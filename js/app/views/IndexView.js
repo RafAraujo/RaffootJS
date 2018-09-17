19a8c420a41813ca209a98115ca50a11e23b9c97
Rafael Araujo <raff-araujo@hotmail.com>
2018-09-16 22:00:03 -0300

    From span to td

--- js/app/views/IndexView.js
+++ js/app/views/IndexView.js
@@ -5,8 +5,8 @@ class IndexView extends View {
         this._game = game;
 
         this._selectDatabases = document.getElementById('databases');
-        this._labelClub = document.getElementById('club');
-        this._labelYear = document.getElementById('year');
+        this._tdClub = document.getElementById('club');
+        this._tdYear = document.getElementById('year');
 
         this._buttonLoad = document.getElementById('load-game');
         this._buttonDelete = document.getElementById('delete-game');
@@ -17,8 +17,8 @@ class IndexView extends View {
             this._fillDatabases();
 
         this._buttonLoad.disabled = this._buttonDelete.disabled = this._game.name.length === 0;
-        this._labelClub.innerText = this._game.clubName;
-        this._labelYear.innerText = this._game.seasonYear;
+        this._tdClub.innerText = this._game.clubName;
+        this._tdYear.innerText = this._game.seasonYear;
     }
 
     _fillDatabases() {
