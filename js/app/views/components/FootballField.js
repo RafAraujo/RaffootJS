7b1da48a9d519849a16aca801aa4851d862f6c69
Rafael Araujo <raff-araujo@hotmail.com>
2018-09-22 12:13:13 -0300

    Football field style

--- js/app/views/components/FootballField.js
+++ js/app/views/components/FootballField.js
@@ -5,11 +5,19 @@ let FootballField = (function () {
             this._container = container;
 
             this._canvas = Html.createElement('canvas', '', 'football-field');
-            this._canvasFontSize = 20;
         }
 
         get _context() {
             return this._canvas.getContext('2d');
+        }
+
+        get _fontFamily() {
+            return this._canvasFontFamily;
+        }
+
+        set _fontFamily(value) {
+            this._canvasFontFamily = value;
+            this._configFont();
         }
 
         get _fontSize() {
@@ -18,7 +26,20 @@ let FootballField = (function () {
 
         set _fontSize(value) {
             this._canvasFontSize = value;
-            this._context.font = `bold ${value}px monospace`;
+            this._configFont();
+        }
+
+        get _bold() {
+            return this._canvasFontBold;
+        }
+
+        set _bold(value) {
+            this._canvasFontBold = value;
+            this._configFont();
+        }
+
+        _configFont() {
+            this._context.font = `${this._canvasFontBold ? 'bold' : ''} ${this._canvasFontSize}px ${this._canvasFontFamily}`;
         }
 
         build() {
@@ -42,29 +63,30 @@ let FootballField = (function () {
             let columnWidth = this._canvas.width / 5;
             let lineHeight = this._canvas.height / 12;
 
+            this._fontFamily = 'Segoe UI';
             this._fontSize = 20;
 
-            let playerWidth = columnWidth * 0.6;
-            let playerHeight = playerWidth;
+            let playerWidth = columnWidth * 0.8;
+            let playerHeight = lineHeight;
 
             this._context.strokeStyle = fl.position.fieldRegion.color.value;
             this._context.fillStyle = fl.position.fieldRegion.color.value;
-            this._context.lineWidth = 2;
-
-            this._context.strokeRect(
-                columnWidth * fl.column + ((columnWidth - playerWidth) / 2),
-                lineHeight * (11 - fl.line) + ((lineHeight - playerHeight) / 2),
-                playerWidth,
-                playerHeight
-            );
+            this._context.lineWidth = 3;
 
             this._context.fillText(
                 text,
                 columnWidth * fl.column + ((columnWidth - this._context.measureText(text).width) / 2),
                 lineHeight * (11 - fl.line) + ((lineHeight - playerHeight) / 2) + (playerHeight / 2)
             );
+            
+            this._fontSize = 14;
 
-            this._fontSize = 18;
+            this._context.strokeRect(
+                columnWidth * fl.column + ((columnWidth - playerWidth) / 2),
+                lineHeight * (11 - fl.line) + ((lineHeight - playerHeight) / 2),
+                playerWidth,
+                playerHeight - this._fontSize - this._context.lineWidth / 2
+            );
 
             this._context.fillStyle = 'white';
             this._context.fillRect(
