f263ee4c0e72ef5c2278ee5418d8bd087b538b40
Rafael Araujo <raff-araujo@hotmail.com>
2018-10-13 23:41:26 -0300

    Defining clubs

--- js/app/models/Club.js
+++ js/app/models/Club.js
@@ -2,23 +2,24 @@ let Club = (function () {
     let _clubs = [];
 
     return class Club extends Entity {
-        constructor(name, countryId, clubColorsId) {
+        constructor(name, countryId, clubColorsId, stadiumId, initialDivision) {
             super();
 
             this.name = name;
             this._countryId = countryId;
             this._clubColorsId = clubColorsId;
-            this._stadiumId = null;
+            this._stadiumId = stadiumId;
             this._coachId = null;
             this._squadId = null;
             this.playable = false;
-            this.initialDivision = null;
+            this.initialDivision = initialDivision;
             this.money = 0;
         }
 
-        static create(name, country) {
-            let clubColors = ClubColors.all().getRandomItem();
-            let club = new Club(name, country.id, clubColors.id);
+        static create(name, country, clubColorsArray, stadiumName, initialDivision) {
+            let clubColors = ClubColors.find(...clubColorsArray);
+            let stadium = Stadium.create(stadiumName, Stadium.baseTicketPrice(initialDivision));
+            let club = new Club(name, country.id, clubColors.id, stadium.id, initialDivision);
             club.id = _clubs.push(club);
             country.addClub(club);
             return club;
@@ -32,11 +33,89 @@ let Club = (function () {
             return _clubs;
         }
 
-        static async seedAsync() {
+        static async seedBrazil() {
+            let countries = Country.playable();
+
+            let argentina = countries[0];
+            Club.create('Atlético Tucumán', argentina, ['DodgerBlue', 'White'], 'Monumental José Fierro', 1);
+            Club.create('Banfield', argentina, ['Green', 'White'], 'Florencio Sola', 1);
+            Club.create('Boca Juniors', argentina, ['DarkBlue', 'Gold'], 'La Bombonera', 1);
+            Club.create('Colón', argentina, ['Red', 'Black'], 'Estanislao López', 1);
+            Club.create('Estudiantes', argentina, ['White', 'Red'], 'Jorge Luis Hirsch', 1);
+            Club.create('Independiente', argentina, ['Red', 'White'], 'Libertadores de América', 1);
+            Club.create('Lanús', argentina, ['DarkRed', 'White'], 'La Fortaleza', 1);
+            Club.create("Newell's Old Boys", argentina, ['Black', 'Red'], 'La Independencia', 1);
+            Club.create('Racing', argentina, ['DodgerBlue', 'White'], 'Juan Perón', 1);
+            Club.create('River Plate', argentina, ['White', 'Red'], 'Monumental de Núñez', 1);
+            Club.create('Rosario Central', argentina, ['DarkBlue', 'Gold'], 'Arroyito', 1);
+            Club.create('San Lorenzo', argentina, ['Coral', 'DarkBlue'], 'Nuevo Gasómetro', 1);
+
+            let brazil = countries[1];
+            Club.create('Atlético-MG', brazil, ['Black', 'White'], 'Independência', 1);
+            Club.create('Botafogo', brazil, ['Black', 'White'], 'Engenhão', 1);
+            Club.create('Corinthians', brazil, ['White', 'Black'], 'Arena Corinthians', 1);
+            Club.create('Cruzeiro', brazil, ['Blue', 'White'], 'Mineirão', 1);
+            Club.create('Flamengo', brazil, ['Red', 'Black'], 'Maracanã', 1);
+            Club.create('Fluminense', brazil, ['DarkRed', 'LightGreen'], 'Laranjeiras', 1);
+            Club.create('Grêmio', brazil, ['DodgerBlue', 'Black'], 'Arena do Grêmio', 1);
+            Club.create('Internacional', brazil, ['Red', 'White'], 'Beira Rio', 1);
+            Club.create('Palmeiras', brazil, ['Green', 'White'], 'Allianz Parque', 1);
+            Club.create('Santos', brazil, ['White', 'Black'], 'Vila Belmiro', 1);
+            Club.create('São Paulo', brazil, ['White', 'DarkRed'], 'Morumbi', 1);
+            Club.create('Vasco da Gama', brazil, ['White', 'Black'], 'São Januário', 1);
+
+            let chile = countries[2];
+            Club.create('Audax Italiano', chile, ['Green', 'White'], 'La Florida', 1);
+            Club.create('Cobreloa', chile, ['Orange', 'White'], 'Zorros del Desierto', 1);
+            Club.create('Cobresal', chile, ['White', 'Orange'], 'El Cobre', 1);
+            Club.create('Colo-Colo', chile, ['White', 'Black'], 'Monumental David Arellano', 1);
+            Club.create('Everton de Viña del Mar', chile, ['DarkBlue', 'Gold'], 'Sausalito', 1);
+            Club.create('Huachipato', chile, ['Black', 'DodgerBlue'], 'Huachipato-CAP Acero', 1);
+            Club.create("O' Higgin's", chile, ['DodgerBlue', 'White'], 'El Teniente', 1);
+            Club.create('Palestino', chile, ['White', 'Green'], 'La Cisterna', 1);
+            Club.create('Santiago Wanderers', chile, ['Green', 'White'], 'Figuero Brander', 1);
+            Club.create('Unión Española', chile, ['Coral', 'Blue'], 'Santa Laura', 1);
+            Club.create('Universidad Católica', chile, ['White', 'Blue'], 'San Carlos', 1);
+            Club.create('Universidad Chile', chile, ['DarkBlue', 'White'], 'Nacional de Chile', 1);
+
+            let colombia = countries[3];
+            Club.create('América de Cali', colombia, ['Red', 'White'], 'Pascual Guerrero', 1);
+            Club.create('Atlético Nacional', colombia, ['Green', 'White'], 'Atanasio Girardot', 1);
+            Club.create('Boyaca Chicó', colombia, ['White', 'Green'], 'La Independencia', 1);
+            Club.create('Deportes Tolima', colombia, ['DarkRed', 'Gold'], 'Manuel Murillo Toro', 1);
+            Club.create('Deportivo Cali', colombia, ['Green', 'White'], 'Deportivo Cali', 1);
+            Club.create('Deportivo Pasto', colombia, ['Blue', 'Coral'], 'Departamental Libertad', 1);
+            Club.create('Independiente Medellín', colombia, ['Coral', 'DarkBlue'], 'Atanasio Girardot', 1);
+            Club.create('Junior Barranquilla', colombia, ['Red', 'White'], 'Metropolitano Roberto Meléndez', 1);
+            Club.create('La Equidad', colombia, ['Green', 'White'], 'Metropolitano de Techo', 1);
+            Club.create('Millonarios', colombia, ['Blue', 'White'], 'El Campín', 1);
+            Club.create('Once Caldas', colombia, ['White', 'Blue'], 'Palogrande', 1);
+            Club.create('Santa Fe', colombia, ['Red', 'White'], 'El Campín', 1);
+
+            let ecuador = countries[4];
+            Club.create('Barcelona Guayaquil', ecuador, ['LightYellow', 'DarkBlue'], 'Monumental Isidro Romero Carbo', 1);
+            Club.create('Emelec', ecuador, ['DodgerBlue', 'White'], 'George Capwell', 1);
+            Club.create('Independiente Del Valle', ecuador, ['Black', 'White'], 'Rumiñahui', 1);
+            Club.create('LDU', ecuador, ['White', 'DarkBlue'], 'Rodrigo Paz Delgado', 1);
+
+            let clubs = Club.all().filter(c => c.country === brazil);
+            for (let i = 0; i < clubs.length; i++) {
+                let c = clubs[i];
+                c.playable = true;
+                c.initialDivision = i + 1;
+                c.stadium.ticketPrice = Stadium.baseTicketPrice(c.initialDivision).id;
+                await c._generateSquadAsync();
+                c.receive(c.squad.wage * Random.numberBetween(6, 9));
+            }
+        }
+
+        static async seedAsyncFromWeb() {
+            await Club.seedBrazil();
+
             try {
                 for (let country of Country.playable()) {
                     let names = await country.getClubNamesAsync();
-                    names.forEach(n => Club.create(n, country));
+                    names.forEach(n => Club.create(n, country, ClubColors.all().getRandomItem()));
 
                     let clubs = country.clubs.slice().shuffle();
                     let divisionCount = country.divisionCount;
--- js/app/models/ClubColors.js
+++ js/app/models/ClubColors.js
@@ -1,8 +1,10 @@
 let ClubColors = (function () {
     let _clubColors = [];
 
-    return class ClubColors {
+    return class ClubColors extends Entity {
         constructor(background, foreground) {
+            super();
+
             this.background = background;
             this.foreground = foreground;
         }
@@ -13,47 +15,19 @@ let ClubColors = (function () {
             return clubColors;
         }
 
+        static load(objects) {
+            _clubColors = objects.map(o => Object.assign(new ClubColors(), o));
+        }
+
         static all() {
-            _clubColors = [];
-
-            ClubColors.create('Black', 'White');
-            ClubColors.create('Black', 'Gold');
-            ClubColors.create('Black', 'LightBlue');
-            ClubColors.create('Black', 'LightGreen');
-            ClubColors.create('Black', 'Salmon');
-            ClubColors.create('Blue', 'Gold');
-            ClubColors.create('Blue', 'White');
-            ClubColors.create('Brown', 'White');
-            ClubColors.create('Cyan', 'Black');
-            ClubColors.create('Crimson', 'Black');
-            ClubColors.create('Crimson', 'White');
-            ClubColors.create('DarkBlue', 'White');
-            ClubColors.create('DarkBlue', 'Gold');
-            ClubColors.create('DarkGreen', 'White');
-            ClubColors.create('DarkRed', 'LightBlue');
-            ClubColors.create('DarkRed', 'White');
-            ClubColors.create('DodgerBlue', 'White');
-            ClubColors.create('Gold', 'Black');
-            ClubColors.create('Gray', 'White');
-            ClubColors.create('Green', 'White');
-            ClubColors.create('GreenYellow', 'Black');
-            ClubColors.create('Khaki', 'Black');
-            ClubColors.create('LightBlue', 'Black');
-            ClubColors.create('LightGreen', 'Black');
-            ClubColors.create('Orange', 'Black');
-            ClubColors.create('OrangeRed', 'White');
-            ClubColors.create('Purple', 'White');
-            ClubColors.create('Red', 'Black');
-            ClubColors.create('Red', 'White');
-            ClubColors.create('RoyalBlue', 'White');
-            ClubColors.create('White', 'Black');
-            ClubColors.create('White', 'Blue');
-            ClubColors.create('White', 'Green');
-            ClubColors.create('White', 'OrangeRed');
-            ClubColors.create('White', 'Purple');
-            ClubColors.create('White', 'Red');
-
             return _clubColors;
+        }
+
+        static find(background, foreground) {
+            let clubColors = ClubColors.all().find(cc => cc.background === background && cc.foreground === foreground);
+            if (!clubColors)
+                clubColors = ClubColors.create(background, foreground);
+            return clubColors;
         }
     }
 })();
